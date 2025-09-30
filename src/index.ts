3540fae761efb0b640c0c0014c3c00a1f45f703f
mugisha steven <126787588+mugishastev@users.noreply.github.com>
2025-09-30 10:58:59 +0200

    port changed

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import mainRouter from "./routes/indexRouting";
 
 const app = express();
 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT; 
 
 // const corsOptions = {
 //     origin: ["http://localhost:5173", "http://localhost:3000"], 
